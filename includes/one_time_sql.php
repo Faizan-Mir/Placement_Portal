50b249ac0f97a0a2c4997fd9a1fe8e5734990701
Mir Faizan <faizan.nitsxr@gmail.com>
2022-10-01 17:00:24 +0530

    Added Several new files

---
+++ includes/one_time_sql.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+// require("botdetect.php");
+
+
+ 
+$servername = "localhost";
+$username = "username";  // Kindly Provide your MySQL username here for this to work on your system. 
+$password = "password";   // Kindly Provide your MySQL Passoword here for this to work on your system. 
+$dbname= "Placement_Database"; 
+//Creating Connection 
+$conn = new mysqli($servername, $username, $password);
+
+//Creating Database named Placement_Database 
+$conn->query('CREATE DATABASE Placement_Database'); 
+
+ Connecting to the Database Placement_Database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+// Creating a Table for our form entries named formEntries
+// This Table contains four columns viz Name, Email, DOB and About_Yourself. 
+
+$conn->query('CREATE TABLE offers (
+Name VARCHAR(100) NOT NULL,
+Enrolment_Number VARCHAR(100) PRIMARY KEY,
+Company_Name VARCHAR(1000) NOT NULL
+)');  
+
+?>
--- index.php
+++ index.php
@@ -22,35 +22,7 @@
 
 <?php
 include('includes/header.php'); 
-session_start();
-// require("botdetect.php");
-
-
- 
-$servername = "localhost";
-$username = "username";  // Kindly Provide your MySQL username here for this to work on your system. 
-$password = "password";   // Kindly Provide your MySQL Passoword here for this to work on your system. 
-$dbname= "Placement_Database"; 
-// Creating Connection 
-$conn = new mysqli($servername, $username, $password);
-
-//Creating Database named Placement_Database 
-$conn->query('CREATE DATABASE Placement_Database'); 
-
-// Connecting to the Database Placement_Database
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-// Creating a Table for our form entries named formEntries
-// This Table contains four columns viz Name, Email, DOB and About_Yourself. 
-$conn->query('CREATE TABLE offers (
-Name VARCHAR(100) NOT NULL,
-Enrolment_Number VARCHAR(100) PRIMARY KEY,
-Company_Name VARCHAR(1000) NOT NULL
-)');  
-
-
-
+require('includes/database_connection.php'); 
 
 
 ?>
@@ -117,8 +89,14 @@ while($row = $result->fetch_assoc()) {
             </form>           
  </div>
 
-</body>
 
+</body>
+<footer>
+
+<?php
+include('includes/footer.php'); 
+?>
+</footer>
 
 
 
